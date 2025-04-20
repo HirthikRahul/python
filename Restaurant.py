64db8555f3153a351a6f130eb6c0276c6f3cc7af
HirthikRahul <hirthikrahul@gmail.com>
2025-04-19 23:31:08 -0500

    need to create object and cart

---
+++ Restaurant.py
@@ -0,0 +1,69 @@
+# first we ask the customer for what do they want( pizza, burger,sandwich)
+# based on the input we create an pizza/burger/sw object
+# step1 : we ask the user for the base and mention the price
+# step2 : then we ask for the toppings
+# step3 : we populate the price along with the tax and add it to the cart along with the item
+
+
+class Cart:
+    def __init__(self,elements):
+        self.cartdict=[]
+        self.cartdict.append(elements)
+        pass
+
+
+'''
+ class Selections:
+
+    foodDict={0:"Burger",
+          1:"Pizza",
+          2:"Sandwich"}
+
+    baseDict={"basic":"$10",
+              "premium":"$20",
+              "luxury":"$30"}
+    
+    toppingsDict={"olives":"$1",
+                  "onions":"$2",
+                  "tomatoes":"$3"}'''
+
+class FoodItem:
+    def __init__(self):
+        pass
+    def get_price(self):
+        pass
+
+class Pizza(FoodItem):
+    def __init__(self):
+        self.foodtype="Pizza"
+        self.base_price=8
+
+    def get_price(self):
+        return self.base_price
+class Burger(FoodItem):
+    def __init__(self):
+        self.foodtype="Burger"
+        self.base_price=6
+    
+    def get_price(self):
+        return self.base_price
+    
+class Sandwich(FoodItem):
+    def __init__(self):
+        self.foodtype="Sandwich"
+        self.base_price=10
+    def get_price(self):
+        return self.base_price
+    
+class Toppings(FoodItem):
+    def __init__(self,fooditem:FoodItem):
+        self.fooditem=fooditem
+  
+
+#drivercode:
+#print("base price is",Selections.baseDict["basic"])
+
+
+
+
+
